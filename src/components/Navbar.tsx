764b6ccbccff5597f6671328bb4ce2921fb16a58
Carlos Leite <carlosebl@gmail.com>
2025-05-19 15:29:28 -0300

    creating server action to load user data

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,29 @@
+import { getUserByEmail } from "@/actions";
 import { auth, signOut } from "auth"
 import Link from "next/link";
 
 const Navbar = async () => {
 
     const session = await auth();
+
+    const user = await getUserByEmail(session?.user?.email);
+
   return (
-    <nav className="bg-gray-800 text-white flex justify-between p-4 items-center">
-        <Link href="/" className="text-white text-lg font-bold">Home</Link>
+    <nav className="bg-gray-800 text-white px-10 py-5 flex justify-between p-4 items-center">
+        <Link href="/" className="text-white hover:text-zinc-200 text-lg font-bold">
+            NextGram
+        </Link>
         <div>
-            {session && session.user ? (
-                <div className="flex items-center">
-                    <p>{session.user.name}</p>
+            {user ? (
+                <div className="flex gap-4 items-center">
+                    <p>{user.name}</p>
                     <form
                         action={async () => {
                             'use server'
                             await signOut()
                         }}
                     >
-                        <button className="bg-red-500 hover:bg-red-700 text-white py-1 px-4 rounded cursor-pointer ml-2">Sair</button>
+                        <button className="bg-red-500 hover:bg-red-700 text-white py-1 px-4 rounded cursor-pointer">Sair</button>
                     </form>
                 </div>
             ) : (
