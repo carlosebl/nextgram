01b9336aee9aa4064428e5c7a86ba9e031208857
Carlos Leite <carlosebl@gmail.com>
2025-05-19 17:03:57 -0300

    finishing navbar

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -6,7 +6,14 @@ type ButtonLinkProps = {
 };
 
 const ButtonLink: React.FC<ButtonLinkProps> = ({ text, url }) => {
-  return <Link href={url} className="w-fit h-8 bg-blue-800 hover:bg-blue-700 rounded cursor-pointer text-sm font-medium text-white py-1 px-6 flex items-center">{text}</Link>;
+  return (
+    <Link
+      href={url}
+      className="w-fit h-8 bg-blue-800 hover:bg-blue-700 rounded cursor-pointer text-sm font-medium text-white py-1 px-6 flex items-center"
+    >
+      {text}
+    </Link>
+  );
 };
 
 export default ButtonLink;
