1682b57f4a476d6b701026e388929162da2e4a48
Carlos Leite <carlosebl@gmail.com>
2025-05-19 17:45:06 -0300

    created user edit form

--- src/actions.ts
+++ src/actions.ts
@@ -1,21 +1,31 @@
-'use server'
+"use server";
 
-import { PrismaClient } from "@prisma/client"
-import { User } from "@prisma/client"
-import path from "path"
+import { PrismaClient } from "@prisma/client";
+import { User } from "@prisma/client";
 
-const prisma = new PrismaClient()
+type FormState = {
+    message: string;
+    type: string
+};
+
+const prisma = new PrismaClient();
 
 export async function getUserByEmail(
-    email: string | null
-    ): Promise<User | null> {
-        if(!email) return null
-        
-        const user = await prisma.user.findFirst({
-            where: {
-                email: email
-            }
-        })
-    return user
+  email: string | null
+): Promise<User | null> {
+  if (!email) return null;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      email: email,
+    },
+  });
+  return user;
 }
 
+export async function updateUserProfile(
+  formState: FormState,
+  formData: FormData
+): Promise<FormState> {
+  return {message: "Perfil atualizado com sucesso!", type: "success"};
+}
--- src/components/ProfileForm.tsx
+++ src/components/ProfileForm.tsx
@@ -1,5 +1,44 @@
-const ProfileForm = ({ user }) => {
-  return <div>ProfileForm</div>;
+"use client";
+
+import { User } from "types/User";
+import { useFormState } from "react-dom";
+import { updateUserProfile } from "@/actions";
+import Label from "./Label";
+import Button from "./Button";
+
+type ProfileFormProps = {
+  user: User;
+};
+
+const ProfileForm: React.FC<ProfileFormProps> = ({ user }) => {
+  const [formState, formActions] = useFormState(updateUserProfile, {
+    message: "",
+    type: "success",
+  });
+
+  return (
+    <div>
+      {formState.message && <p>Algum texto...</p>}
+      <form>
+        <input type="hidden" name="id" value={user.id} />
+        <div>
+          <Label htmlFor="name" text="Nome" />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Digite seu nome"
+            defaultValue={user.name || ""}
+            className="p-2 border border-zinc-300 rounded w-full text-sm placeholder:text-zinc-500 fovus:ring-0 focus:outline-none"
+          />
+        </div>
+        <p>Image preview</p>
+        <div className="flex justify-end">
+            <Button type="submit" text="Salvar" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ProfileForm;
